2dcae2e6f329fdcd05b2eb7045215be7bb144604
soydanino <juan-galvis@outlook.com>
2026-07-11 22:23:08 -0500

    Remove smooth scroll functionality from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Music from './components/Music';
@@ -6,31 +6,6 @@ import Tours from './components/Tours';
 import Footer from './components/Footer';
 
 function App() {
-  useEffect(() => {
-    // Smooth Scroll
-    const handleAnchorClick = function (e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: 'smooth'
-        });
-      }
-    };
-
-    const anchors = document.querySelectorAll('a[href^="#"]');
-    anchors.forEach(anchor => {
-      anchor.addEventListener('click', handleAnchorClick);
-    });
-
-    return () => {
-      anchors.forEach(anchor => {
-        anchor.removeEventListener('click', handleAnchorClick);
-      });
-    };
-  }, []);
-
   return (
     <>
       <Navbar />
